a1752a5a150bf8ab6324d914cbeb1492e8476da7
pelittomer <pelittomer99@gmail.com>
2025-07-26 10:51:55 +0300

    feat(upload): Implement upload management.

--- backend/src/main/java/com/online_store/backend/api/upload/controller/UploadController.java
+++ backend/src/main/java/com/online_store/backend/api/upload/controller/UploadController.java
@@ -3,11 +3,18 @@ package com.online_store.backend.api.upload.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.online_store.backend.api.upload.entities.Upload;
 import com.online_store.backend.api.upload.service.UploadService;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/upload")
@@ -16,9 +23,17 @@ public class UploadController {
     private final UploadService uploadService;
 
     @GetMapping("/{id}")
-    public String getUploadById(@RequestParam String param) {
-        // This function retrieves an image by its unique ID.
-        return new String();
+    public ResponseEntity<Resource> getUploadById(@PathVariable Long id) {
+        Upload uploadFile = uploadService.getUploadById(id);
+        Resource resource = new ByteArrayResource(uploadFile.getFileContent());
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + uploadFile.getFileName() + "\"");
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(uploadFile.getFileType()))
+                .headers(headers)
+                .body(resource);
     }
 
 }
