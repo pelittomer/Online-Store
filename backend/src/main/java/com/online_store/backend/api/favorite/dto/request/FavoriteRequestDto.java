a0bda98dc5b3c9d6f1ea408747c992a8d75ef538
pelittomer <pelittomer99@gmail.com>
2025-07-27 13:11:56 +0300

    feat(favorite): Implement validation for Favorite DTOs.

--- backend/src/main/java/com/online_store/backend/api/favorite/dto/request/FavoriteRequestDto.java
+++ backend/src/main/java/com/online_store/backend/api/favorite/dto/request/FavoriteRequestDto.java
@@ -1,5 +1,7 @@
 package com.online_store.backend.api.favorite.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,5 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FavoriteRequestDto {
+    @NotNull(message = "Product ID cannot be null.")
+    @Min(value = 1, message = "Product ID must be a positive number.")
     private Long product;
 }
