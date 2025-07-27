01a271fe6ff9114478736600aa11680fbd87a9ce
pelittomer <pelittomer99@gmail.com>
2025-07-27 13:36:43 +0300

    feat(return-request): Implement validation for ReturnRequest DTOs.

--- backend/src/main/java/com/online_store/backend/api/returnRequest/dto/request/ReturnRequestDto.java
+++ backend/src/main/java/com/online_store/backend/api/returnRequest/dto/request/ReturnRequestDto.java
@@ -1,5 +1,9 @@
 package com.online_store.backend.api.returnRequest.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +14,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReturnRequestDto {
+    @NotBlank(message = "Reason cannot be blank.")
+    @Size(min = 10, max = 500, message = "Reason must be between 10 and 500 characters.")
     private String reason;
+
+    @NotNull(message = "Quantity cannot be null.")
+    @Min(value = 1, message = "Quantity must be at least 1.")
     private Integer quantity;
+
+    @NotNull(message = "Order item ID cannot be null.")
+    @Min(value = 1, message = "Order item ID must be a positive number.")
     private Long orderItem;
 }
--- backend/src/main/java/com/online_store/backend/api/returnRequest/dto/request/UpdateReturnRequestDto.java
+++ backend/src/main/java/com/online_store/backend/api/returnRequest/dto/request/UpdateReturnRequestDto.java
@@ -2,6 +2,8 @@ package com.online_store.backend.api.returnRequest.dto.request;
 
 import com.online_store.backend.api.order.entities.OrderStatus;
 
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +14,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateReturnRequestDto {
+    @NotNull(message = "Status cannot be null.")
     private OrderStatus status;
+    
     private String rejectionReason;
+
+    @AssertTrue(message = "Rejection reason must be provided when the status is 'REJECTED'.")
+    private boolean isRejectionReasonProvided() {
+        if (status == OrderStatus.RETURN_REJECTED) {
+            return rejectionReason != null && !rejectionReason.isBlank();
+        }
+        return true;
+    }
 }
