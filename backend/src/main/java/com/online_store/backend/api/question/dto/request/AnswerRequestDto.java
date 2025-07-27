f4a7c5484eab8b3c43ce9f35b75850ad82ff7f68
pelittomer <pelittomer99@gmail.com>
2025-07-27 13:32:09 +0300

    feat(question): Implement validation for Question DTOs.

--- backend/src/main/java/com/online_store/backend/api/question/dto/request/AnswerRequestDto.java
+++ backend/src/main/java/com/online_store/backend/api/question/dto/request/AnswerRequestDto.java
@@ -1,5 +1,7 @@
 package com.online_store.backend.api.question.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,5 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AnswerRequestDto {
+    @NotBlank(message = "Answer cannot be blank.")
+    @Size(min = 10, max = 500, message = "Answer must be between 10 and 500 characters.")
     private String answer;
 }
--- backend/src/main/java/com/online_store/backend/api/question/dto/request/QuestionRequestDto.java
+++ backend/src/main/java/com/online_store/backend/api/question/dto/request/QuestionRequestDto.java
@@ -1,5 +1,9 @@
 package com.online_store.backend.api.question.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +14,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class QuestionRequestDto {
+    @NotBlank(message = "Question cannot be blank.")
+    @Size(min = 10, max = 255, message = "Question must be between 10 and 255 characters.")
     private String question;
+
+    @NotNull(message = "Product ID cannot be null.")
+    @Min(value = 1, message = "Product ID must be a positive number.")
     private Long product;
 }
